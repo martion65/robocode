02f1eac445ebe32dbf5fc029337863a7ccb7f513
martion65 <tredman.uk@outlook.com>
2026-03-25 17:01:58 +0000

    interaction

--- scripts/player.cs
+++ scripts/player.cs
@@ -5,10 +5,25 @@ public partial class player : CharacterBody2D
 {
 	public const float Speed = 150.0f;
 	public const float JumpVelocity = -200.0f;
+	
+	private bool isInRange = false;
+	private Node2D targetObject;
+	private CharacterBody2D heldObject;
+
+	private Marker2D handPosition;
+	
+	public override void _Ready()
+	{
+		handPosition = GetNode<Marker2D>("handposition");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
+		PickupObject();
+		DropObject();
+		
 		Vector2 velocity = Velocity;
+		
 
 		// Add the gravity.
 		if (!IsOnFloor())
@@ -36,5 +51,56 @@ public partial class player : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+	
+	private void PickupObject()
+	{
+		if (isInRange && Input.IsActionJustPressed("pickup") && heldObject == null)
+		{
+			heldObject = (CharacterBody2D)targetObject;
+			
+			heldObject.Reparent(handPosition);
+			heldObject.Position = Vector2.Zero; 
+		}
+	}
+
+	private void DropObject()
+	{
+		if (heldObject == null) return;
+
+		if (Input.IsActionJustPressed("drop right"))
+		{
+			PerformDrop(Vector2.Right * 10);
+		}
+		else if (Input.IsActionJustPressed("drop left"))
+		{
+			PerformDrop(Vector2.Left * 10);
+		}
+	}
+
+	private void PerformDrop(Vector2 offset)
+	{
+		heldObject.Reparent(GetParent());
+		heldObject.Position = Position + offset;
+		heldObject = null;
+	}
+
+   
+	private void _on_area_2d_body_entered(Node2D body)
+	{
+		if (body is Pickable)
+		{
+			isInRange = true;
+			targetObject = body;
+		}
+	}
+
+	private void _on_area_2d_body_exited(Node2D body)
+	{
+		if (body is Pickable)
+		{
+			isInRange = false;
+			targetObject = null;
+		}
 	}
 }
